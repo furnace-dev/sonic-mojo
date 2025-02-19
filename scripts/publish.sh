aa55c27a72114ceaacc38018437d80bc7a52fdcb
yl <2201441955@qq.com>
2025-02-19 22:23:26 +0800

    update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,4 +7,4 @@ for file in $CONDA_BLD_PATH/**/*.conda; do
     magic run rattler-build upload prefix -c "furnace-dev" "$file" --api-key=$PREFIX_DEV_API_KEY || true
 done
 
-# rm $CONDA_BLD_PATH/**/*.conda
+rm $CONDA_BLD_PATH/**/*.conda
